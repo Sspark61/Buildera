af64e98fca5b39330ae75191047e608c5c064fd8
Mahmoud Abdelnaby <mahmoud.oshy12@gmail.com>
2026-05-11 05:17:58 +0300

    feat: adding font to login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/space-grotesk/700.css'
 import './index.css'
 import Login from './pages/login-page/login.tsx'
 
--- src/pages/login-page/login.tsx
+++ src/pages/login-page/login.tsx
@@ -8,13 +8,13 @@ export default function login() {
         <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen bg-[#12151c]">
             <div className="hidden lg:block relative h-full bg-cover bg-center opacity-60" style={{ backgroundImage: `url(${loginImg})` }}>
                 <div className="bottom-12 left-12 absolute">
-                    <h1 className='text-3xl font-bold'>Engineer <br /> Your Reality.</h1>
-                    <p>Build the machine of your dreams with Buildera.</p>
+                    <h1 className='text-3xl font-bold font-heading'>Engineer <br /> Your Reality.</h1>
+                    <p className='text-sm'>Build the machine of your dreams with Buildera.</p>
                 </div>
             </div>
-            <div className="login grid place-content-center text-center">
-                <a href="" className='logo inline-flex items-center justify-center gap-2 mb-4'><img className='w-1/10' src="/src/assets/buildera-logo.png" alt="" /><span>Buildera</span></a>
-                <h3 className='text-xl'>Welcome back</h3>
+            <div className="login grid place-content-center text-center ">
+                <a href="" className='logo inline-flex items-center justify-center gap-2 mb-4'><img className='w-1/10' src="/src/assets/buildera-logo.png" alt="" /><span className='font-heading text-bold'>Buildera</span></a>
+                <h3 className='text-xl font-heading'>Welcome back</h3>
                 <p className='text-sm text-gray-400 pb-4'>Log in to your account</p>
                 <div>
                     <Formik
@@ -31,6 +31,9 @@ export default function login() {
                             if (!values.password) {
                                 errors.password = '*This field is required';
                             }
+                            else if (values.password.length < 8) {
+                                errors.password = "*Password must be at least 8 characters"
+                            }
                             return errors;
                         }}
                         onSubmit={(values, { setSubmitting }) => {
@@ -42,21 +45,27 @@ export default function login() {
                     >
                         {({ isSubmitting }) => (
                             <Form className='space-y-4 pt-4 w-5/6 grid mx-auto'>
-                                <div className='flex items-center bg-[#12151c] border border-white/10 rounded-md px-3 py-2 sm:py-3 focus-within:border-[#4e78da] focus-within:shadow-lg focus-within:[#4e78da] transition '>
+                                <div className='flex items-center bg-(--background) border border-(--border) rounded-md px-3 py-2 sm:py-3 focus-within:border-[#4e78da] focus-within:shadow-lg focus-within:[#4e78da] transition '>
                                     <Mail size={18} strokeWidth={1.5} color="gray" />
-                                    <Field type="email" name="email" placeholder="Enter your Email" className="ps-2 bg-transparent outline-none w-full text-white placeholder:text-slate-500 text-sm" />
+                                    <Field type="email" name="email" placeholder="Email address" className="ps-2 bg-transparent outline-none w-full text-white placeholder:text-slate-500 text-sm" />
                                 </div>
                                 <ErrorMessage name="email" component="div" className='text-start text-red-700 text-sm' />
-                                <div className='flex items-center bg-[#12151c] border border-white/10 rounded-md px-3 py-2 sm:py-3 focus-within:border-[#4e78da] focus-within:shadow-lg focus-within:[#4e78da] transition '>
+                                <div className='flex items-center bg-(--background) border border-(--border) rounded-md px-3 py-2 sm:py-3 focus-within:border-[#4e78da] focus-within:shadow-lg focus-within:[#4e78da] transition '>
                                     <Lock size={18} strokeWidth={1.5} color="gray" />
-                                    <Field type="password" name="password" placeholder="Enter your password" className="ps-2 bg-transparent outline-none w-full text-white placeholder:text-slate-500 text-sm" />
+                                    <Field type="password" name="password" placeholder="Password" className="ps-2 bg-transparent outline-none w-full text-white placeholder:text-slate-500 text-sm" />
 
                                 </div>
                                 <ErrorMessage name="password" component="div" className='text-start text-red-700 text-sm ' />
-                                <button type="submit" disabled={isSubmitting} className='border w-full rounded-md py-2 bg-[#4e78da] hover:bg-blue-400 cursor-pointer transition-all'>
+                                <button type="submit" disabled={isSubmitting} className='border w-full rounded-md py-2 text-sm text-bold bg-(--primary) hover:bg-[#395cae] cursor-pointer transition-all'>
                                     Log in
                                 </button>
-                                <span className='OR text-sm'>or</span>
+                                <div className="flex items-center gap-4 w-full">
+                                    <div className="h-px flex-1 bg-white/10"></div>
+
+                                    <span className="text-gray-400 text-xs">or</span>
+
+                                    <div className="h-px flex-1 bg-white/10"></div>
+                                </div>
                                 <button type="submit" className='flex justify-center items-center mx-auto text-center border w-full rounded-md py-2 hover:bg-gray-800 cursor-pointer transition-all'>
                                     <FcGoogle size={16} />
                                     <span className='ps-2 text-sm'>Google</span>
