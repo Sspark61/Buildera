ed3ac1a8319b7fed78f48ca874b60a4f0e299b0d
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2026-05-12 15:49:54 +0300

    feat: refactor App component to include routing and context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { Button } from "@/components/ui/button"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Login from "./pages/login-page/login.tsx";
+import Signup from "./pages/signup/Signup.tsx";
 
-function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
-}
-
-export default App
+ 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    },
+  },
+});
+ 
+const router = createBrowserRouter([
+  { path: "/login",             element: <Login /> },
+  { path: "/signup",            element: <Signup /> }
+]);
+ 
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <RouterProvider router={router} />
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+ 
+export default App;
