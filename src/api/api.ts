c9914fca365fa12f4a823b61c6f84716e4c494f1
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2026-06-10 16:29:32 +0300

    feat: add API utility for handling requests with authentication

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,15 @@
 const BaseURL = 'https://build-era-kappa.vercel.app'
 
-export const api = async (endPoint , options = {}) => {
+export const api = async (endPoint: string , options: RequestInit = {}) => {
     const token = localStorage.getItem('token')
 
     const res = await fetch(`${BaseURL}${endPoint}`, {
         headers: {
             'Content-Type': 'application/json',
             ...(token && {'Authorization': `access ${token}`})
-        }
+        },
+        ...options
     })
-}
 
 if (res.status === 401) {
     localStorage.removeItem('token')
@@ -22,4 +22,4 @@ if (!res.ok) {
     throw new Error(error.message || 'Something went wrong')
 }
 
-return res.json()
+return res.json()}
