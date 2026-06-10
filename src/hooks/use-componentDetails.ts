2992df8a0963e4f49510d623ef92e66f11c2cefb
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2026-06-10 21:38:36 +0300

    feat: add custom hook for fetching component details by ID

---
+++ src/hooks/use-componentDetails.ts
@@ -0,0 +1,45 @@
+// hooks/useComponents.ts — add this alongside useGetComponents
+import { useQuery } from '@tanstack/react-query';
+import {api} from '../api/api'
+
+interface SingleComponentResponse {
+    success: boolean
+    data: {
+        id: number
+        name: string
+        type: string
+        brand: string
+        price: number | null
+        imageUrl: string
+        createdAt: string
+        updatedAt: string
+        specs: {
+            Series: string,
+            Microarchitecture: string,
+            Core_Family: string,
+            Socket: string,
+            Core_Count: string,
+            Thread_Count: string,
+            Performance_Core_Clock: string,
+            Performance_Core_Boost_Clock: string,
+            L2_Cache: string,
+            L3_Cache: string,
+            TDP: string,
+            Integrated_Graphics: string,
+            ECC_Support: string,
+            Includes_Cooler: string,
+            Packaging: string,
+            Lithography: string,
+            Includes_CPU_Cooler: string,
+            Simultaneous_Multithreading: string
+        }
+    }
+}
+
+export const useGetComponentDetails = (id: number) => {
+    return useQuery({
+        queryKey: ['components', id],
+        queryFn: () => api(`/components/${id}`) as Promise<SingleComponentResponse>,
+        enabled: !!id,
+    })
+}
