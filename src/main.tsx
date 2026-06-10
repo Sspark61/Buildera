d612501e39f6cc2394b1d046573fb1635bb6e863
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2026-06-10 16:09:07 +0300

    feat: implement API utility and integrate React Query in main application

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import App from './App.tsx'
 import '@fontsource/space-grotesk/700.css'
 import './index.css'
 import { ThemeProvider } from "@/hooks/use-theme"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 )
