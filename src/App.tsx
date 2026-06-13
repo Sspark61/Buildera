266b0d9c896d2326afa2696346e116a297bdaada
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2026-06-13 14:18:02 +0300

    fix: update reset password route to remove token parameter

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
   { path: "/forgotPassword", element: <ForgotPassword /> },
-  { path: "/reset-password/:token", element: <ResetPassword /> },
+  { path: "/reset-password", element: <ResetPassword /> },
   { path: "*", element: <NotFound /> }
 ]);
 
